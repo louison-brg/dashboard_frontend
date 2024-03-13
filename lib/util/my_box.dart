4a4c04140cea70d435027264e2f2b7af122832c3
virgile <virgile.louvel@gmail.com>
2024-03-13 09:00:31 +0100

    photo

--- lib/util/my_box.dart
+++ lib/util/my_box.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:dashboard/constant.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -7,16 +9,27 @@ class MyBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: const BoxDecoration(
-        color: Colors.blueGrey,
-        borderRadius: BorderRadius.all(
-          Radius.circular(5),
+    return Padding(
+      padding: const EdgeInsets.all(20.0), // Ajouter un espace de 8 pixels autour du conteneur
+      child: Container(
+        decoration: BoxDecoration(
+          color: const Color(0xFF333232),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: const Center(
+          child: FittedBox(
+            fit: BoxFit.scaleDown,
+            child: Text(
+                'Info',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 18,
+                  color: Color(0xFFFF0000),
+                ),),
+          ),
+
         ),
       ),
-      padding: const EdgeInsets.all(20.0),
-      child: const Text('Info'),
-      alignment: Alignment.center,
     );
   }
 }
